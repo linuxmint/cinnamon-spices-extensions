3e6b999cbc94e429fd73a0f5eaaf666c0e96eec7
Odyseus <venusyadonis@gmail.com>
2017-06-05 17:44:11 -0300

    Window demands attention behavior (0dyseus@WindowDemandsAttentionBehavior) update
    - Changed **var** statements to **let**.
    - Changed tabs to spaces. This was overdue. I changed to spaces in all my xlets more than a year
    ago.
    - Added Chinese localization. Thanks to [giwhub](https://github.com/giwhub).
    - Added Swedish localization. Thanks to [eson57](https://github.com/eson57).
    - Updated localization template and localizations.
    - Changed the use of cookies for **localStorage** on the language selection function of the help files. As a fallback, cookies will still be used if **localStorage** isn't available.
    - Changed the *contributors* key on the metadata.json files to a generic message and moved the contributors to a file. This was to avoid the constant need to update the localizations every time a contributor/mention was added.
    - Redesigned the help file to include the CONTRIBUTORS.md and CHANGELOG.md files. This files will not be included in the Spices repository.
    - Removed all files from outside the xlet folder that aren't used by the Spices repository.

--- 0dyseus@WindowDemandsAttentionBehavior/files/0dyseus@WindowDemandsAttentionBehavior/extension.js
+++ 0dyseus@WindowDemandsAttentionBehavior/files/0dyseus@WindowDemandsAttentionBehavior/extension.js
@@ -5,7 +5,7 @@ const Settings = imports.ui.settings;
 const Mainloop = imports.mainloop;
 
 let CONNECTION_IDS = {
-	WDAE: 0
+    WDAE: 0
 };
 
 const SHORTCUT_ID = "window-demands-attention-extension";
@@ -13,109 +13,109 @@ const SHORTCUT_ID = "window-demands-attention-extension";
 let changeSettingsTimeout;
 
 const WindowDemandsAttentionExtension = new Lang.Class({
-	Name: "WindowDemandsAttentionExtension",
+    Name: "WindowDemandsAttentionExtension",
 
-	_init: function() {
-		this.settings = new Settings.ExtensionSettings(this, "0dyseus@WindowDemandsAttentionBehavior");
-		this.settings.bindProperty(Settings.BindingDirection.IN,
-			"pref_keyboard_shortcut", "pref_keyboard_shortcut", this._toggleEnabled);
-		this.settings.bindProperty(Settings.BindingDirection.IN,
-			"pref_activation_mode", "pref_activation_mode", this._toggleEnabled);
+    _init: function() {
+        this.settings = new Settings.ExtensionSettings(this, "0dyseus@WindowDemandsAttentionBehavior");
+        this.settings.bindProperty(Settings.BindingDirection.IN,
+            "pref_keyboard_shortcut", "pref_keyboard_shortcut", this._toggleEnabled);
+        this.settings.bindProperty(Settings.BindingDirection.IN,
+            "pref_activation_mode", "pref_activation_mode", this._toggleEnabled);
 
-		if (this.pref_activation_mode === "hotkey") {
-			this._windows = [];
+        if (this.pref_activation_mode === "hotkey") {
+            this._windows = [];
 
-			CONNECTION_IDS.WDAE = global.display.connect(
-				"window-demands-attention",
-				Lang.bind(this, this._on_window_demands_attention)
-			);
-		} else if (this.pref_activation_mode === "force") {
-			this._tracker = Cinnamon.WindowTracker.get_default();
-			this._handlerid = global.display.connect("window-demands-attention",
-				Lang.bind(this, this._on_window_demands_attention));
-		}
-	},
+            CONNECTION_IDS.WDAE = global.display.connect(
+                "window-demands-attention",
+                Lang.bind(this, this._on_window_demands_attention)
+            );
+        } else if (this.pref_activation_mode === "force") {
+            this._tracker = Cinnamon.WindowTracker.get_default();
+            this._handlerid = global.display.connect("window-demands-attention",
+                Lang.bind(this, this._on_window_demands_attention));
+        }
+    },
 
-	_toggleEnabled: function() {
-		disable();
-		if (changeSettingsTimeout !== null)
-			Mainloop.source_remove(changeSettingsTimeout);
+    _toggleEnabled: function() {
+        disable();
+        if (changeSettingsTimeout !== null)
+            Mainloop.source_remove(changeSettingsTimeout);
 
-		changeSettingsTimeout = Mainloop.timeout_add(500, Lang.bind(this, enable));
-	},
+        changeSettingsTimeout = Mainloop.timeout_add(500, Lang.bind(this, enable));
+    },
 
-	_on_window_demands_attention: function(aDisplay, aWin) {
-		if (this.pref_activation_mode === "hotkey") {
-			this._windows.push(aWin);
-		} else if (this.pref_activation_mode === "force") {
-			Main.activateWindow(aWin);
-		}
+    _on_window_demands_attention: function(aDisplay, aWin) {
+        if (this.pref_activation_mode === "hotkey") {
+            this._windows.push(aWin);
+        } else if (this.pref_activation_mode === "force") {
+            Main.activateWindow(aWin);
+        }
 
-	},
+    },
 
-	_activate_last_window: function() {
-		if (this._windows.length === 0) {
-			Main.notify("No windows in the queue.");
-			return;
-		}
+    _activate_last_window: function() {
+        if (this._windows.length === 0) {
+            Main.notify("No windows in the queue.");
+            return;
+        }
 
-		let last_window = this._windows.pop();
-		Main.activateWindow(last_window);
-	},
+        let last_window = this._windows.pop();
+        Main.activateWindow(last_window);
+    },
 
-	_add_keybindings: function() {
-		Main.keybindingManager.addHotKey(
-			SHORTCUT_ID,
-			this.pref_keyboard_shortcut,
-			Lang.bind(this, this._activate_last_window));
-	},
+    _add_keybindings: function() {
+        Main.keybindingManager.addHotKey(
+            SHORTCUT_ID,
+            this.pref_keyboard_shortcut,
+            Lang.bind(this, this._activate_last_window));
+    },
 
-	_remove_keybindings: function() {
-		Main.keybindingManager.removeHotKey(SHORTCUT_ID);
-	},
+    _remove_keybindings: function() {
+        Main.keybindingManager.removeHotKey(SHORTCUT_ID);
+    },
 
-	enable: function() {
-		if (this.pref_activation_mode === "hotkey")
-			this._add_keybindings();
-	},
+    enable: function() {
+        if (this.pref_activation_mode === "hotkey")
+            this._add_keybindings();
+    },
 
-	_destroy: function() {
-		try {
-			global.display.disconnect(this._handlerid);
-		} catch (aErr) {}
+    _destroy: function() {
+        try {
+            global.display.disconnect(this._handlerid);
+        } catch (aErr) {}
 
-		try {
-			global.display.disconnect(CONNECTION_IDS.WDAE);
-		} catch (aErr) {}
+        try {
+            global.display.disconnect(CONNECTION_IDS.WDAE);
+        } catch (aErr) {}
 
-		CONNECTION_IDS.WDAE = 0;
-		this._windows = null;
-		this._remove_keybindings();
-	}
+        CONNECTION_IDS.WDAE = 0;
+        this._windows = null;
+        this._remove_keybindings();
+    }
 });
 
 let wdae = null;
 
 function enable() {
-	try {
-		wdae = new WindowDemandsAttentionExtension();
-		wdae.enable();
-	} catch (aErr) {
-		Main.notify("[Window demands attention behavior]", aErr.message);
-		global.logError(aErr);
-	}
+    try {
+        wdae = new WindowDemandsAttentionExtension();
+        wdae.enable();
+    } catch (aErr) {
+        Main.notify("[Window demands attention behavior]", aErr.message);
+        global.logError(aErr);
+    }
 }
 
 function disable() {
-	if (changeSettingsTimeout !== null)
-		Mainloop.source_remove(changeSettingsTimeout);
+    if (changeSettingsTimeout !== null)
+        Mainloop.source_remove(changeSettingsTimeout);
 
-	if (wdae !== null) {
-		wdae._destroy();
-		wdae = null;
-	}
+    if (wdae !== null) {
+        wdae._destroy();
+        wdae = null;
+    }
 }
 
 function init() {
-	//
+    //
 }
