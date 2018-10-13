20277e5e31162464c13d0ba61c58b48a02cdc7b5
Jason Hicks <jaszhix@gmail.com>
2018-10-13 00:39:42 -0500

    Misc clean up
    - Add eslintrc, fix warnings
    - Minor formatting for readability
    - Update deprecated Clutter API usage
    - Use arrow functions where possible for older Spidermonkey engines

--- DesktopCube@yare/files/DesktopCube@yare/extension.js
+++ DesktopCube@yare/files/DesktopCube@yare/extension.js
@@ -9,10 +9,13 @@ const Tweener = imports.ui.tweener;
 const Settings = imports.ui.settings;
 
 let settings;
-let bindings = ['switch-to-workspace-left',
-                'switch-to-workspace-right',
-                'move-to-workspace-left',
-                'move-to-workspace-right'];
+let bindings = [
+    'switch-to-workspace-left',
+    'switch-to-workspace-right',
+    'move-to-workspace-left',
+    'move-to-workspace-right'
+];
+
 
 function Cube() {
     this._init.apply(this, arguments);
@@ -26,18 +29,19 @@ Cube.prototype = {
         this.destroy_requested = false;
         this.monitor = Main.layoutManager.primaryMonitor;
 
-        let [binding_type,,,direction] = binding.get_name().split('-');
+        let [binding_type, , , direction] = binding.get_name().split('-');
         direction = Meta.MotionDirection[direction.toUpperCase()];
         this.direction = direction;
         this.last_direction = direction;
 
-        if (direction != Meta.MotionDirection.RIGHT &&
-            direction != Meta.MotionDirection.LEFT)
+        if (direction !== Meta.MotionDirection.RIGHT &&
+            direction !== Meta.MotionDirection.LEFT)
             return;
 
         let active_workspace = global.screen.get_active_workspace();
         let new_workspace = active_workspace.get_neighbor(direction);
-        if (active_workspace.index() == new_workspace.index())
+
+        if (active_workspace.index() === new_workspace.index())
             return;
 
         this.actor = new St.Group({
@@ -46,14 +50,13 @@ Cube.prototype = {
             y: 0,
             width: global.screen_width,
             height: global.screen_height,
-            visible: true });
+            visible: true
+        });
 
-        Main.uiGroup.add_actor(this.actor);
+        Main.uiGroup.add_child(this.actor);
 
-        this.actor.connect('key-release-event',
-            Lang.bind(this, this._keyReleaseEvent));
-        this.actor.connect('key-press-event',
-            Lang.bind(this, this._keyPressEvent));
+        this.actor.connect('key-release-event', Lang.bind(this, this._keyReleaseEvent));
+        this.actor.connect('key-press-event', Lang.bind(this, this._keyPressEvent));
 
         this.initBackground();
         this.dimBackground();
@@ -61,33 +64,30 @@ Cube.prototype = {
         Main.pushModal(this.actor);
 
         let mask = binding.get_mask();
-        this._modifierMask =
-            imports.ui.appSwitcher.appSwitcher.primaryModifier(mask);
+        this._modifierMask = imports.ui.appSwitcher.appSwitcher.primaryModifier(mask);
         global.window_group.hide();
 
         Main.getPanels().forEach(function(panel){panel.actor.opacity = 0;});
 
-        if (binding_type == "move" &&
-            window.get_window_type() !== Meta.WindowType.DESKTOP)
-                this.moveWindow(window, direction);
+        if (binding_type === "move" && window.get_window_type() !== Meta.WindowType.DESKTOP)
+            this.moveWindow(window, direction);
         this.startAnimate(direction);
         this.actor.show();
     },
 
     removeWindowActor: function(workspace_clone, window, index) {
-        if (workspace_clone && (workspace_clone.index == index)) {
+        if (workspace_clone && (workspace_clone.index === index)) {
             let i = workspace_clone.workspaceWindows.indexOf(window);
-            if (i == -1) return false;
+            if (i === -1) return false;
             let j;
             let done = false;
             for (j = 0; j < workspace_clone.workspaceWindows.length &&
                 !done; j++) {
-                if (window.get_stable_sequence() ==
+                if (window.get_stable_sequence() ===
                     workspace_clone.workspaceWindowActors[j].i)
                     done = true;
             }
-            workspace_clone.remove_actor
-                (workspace_clone.workspaceWindowActors[j-1]);
+            workspace_clone.remove_child(workspace_clone.workspaceWindowActors[j-1]);
             workspace_clone.workspaceWindows.splice(i, 1);
             workspace_clone.workspaceWindowActors.splice(j-1, 1)[0].destroy();
             return true;
@@ -96,27 +96,25 @@ Cube.prototype = {
     },
 
     addWindowActor: function(workspace_clone, window, index) {
-        if (workspace_clone && (workspace_clone.index == index)) {
+        if (workspace_clone && (workspace_clone.index === index)) {
             let windowClone = this.cloneMetaWindow(window);
-            workspace_clone.add_actor(windowClone);
+            workspace_clone.add_child(windowClone);
             //windowClone.raise_top();
             //workspace_clone.chromeGroup.raise_top();
             workspace_clone.workspaceWindowActors.push(windowClone);
             workspace_clone.workspaceWindows.push(window);
-            workspace_clone.workspaceWindows.sort
-                (Lang.bind(this, this._sortWindow));
+            workspace_clone.workspaceWindows.sort(Lang.bind(this, this._sortWindow));
             return true;
         }
         return false;
     },
 
     sortWindowClones: function (workspace_clone) {
-        workspace_clone.workspaceWindowActors.sort(Lang.bind(this,
-            function(actor1, actor2) {
-                let time = this._sortWindow(actor1.win, actor1.win);
-                time > 0 ? actor1.raise(actor2) : actor2.raise(actor1);
-                return 0;
-            }));
+        workspace_clone.workspaceWindowActors.sort((actor1, actor2) => {
+            let time = this._sortWindow(actor1.win, actor1.win);
+            time > 0 ? actor1.raise(actor2) : actor2.raise(actor1);
+            return 0;
+        });
         workspace_clone.chromeGroup.raise_top();
     },
 
@@ -146,14 +144,13 @@ Cube.prototype = {
         let new_index = new_workspace.index();
 
         window.change_workspace(new_workspace);
-        Mainloop.idle_add(Lang.bind(this, function() {
+        Mainloop.idle_add(() => {
             // Unless this is done a bit later,
             // window is sometimes not activated
-            if (window.get_workspace() ==
-                global.screen.get_active_workspace()) {
+            if (window.get_workspace() === global.screen.get_active_workspace()) {
                 window.activate(global.get_current_time());
             }
-        }));
+        });
 
         this.moveWindowClone(window, active_index, new_index);
         return true;
@@ -171,20 +168,16 @@ Cube.prototype = {
         clone.set_size(global.stage.width, global.stage.height);
 
         let background = new St.Group();
-        background.add_actor
-            (Meta.BackgroundActor.new_for_screen(global.screen));
-        clone.add_actor(background);
+        background.add_child(Meta.BackgroundActor.new_for_screen(global.screen));
+        clone.add_child(background);
 
-        let deskletClone =
-            new Clutter.Clone({source : Main.deskletContainer.actor});
-        clone.add_actor(deskletClone);
+        let deskletClone = new Clutter.Clone({source : Main.deskletContainer.actor});
+        clone.add_child(deskletClone);
 
         clone.desktopClones = [];
         global.get_window_actors().forEach(function(w){
-            if(w.get_meta_window().get_window_type() ==
-               Meta.WindowType.DESKTOP) {
-                let texture =
-                    w.get_meta_window().get_compositor_private().get_texture();
+            if (w.get_meta_window().get_window_type() === Meta.WindowType.DESKTOP) {
+                let texture = w.get_meta_window().get_compositor_private().get_texture();
                 let rect = w.get_meta_window().get_input_rect();
                 let windowClone = new Clutter.Clone(
                     {source: texture,
@@ -193,7 +186,7 @@ Cube.prototype = {
                      y: rect.y,
                     });
 
-                clone.add_actor(windowClone);
+                clone.add_child(windowClone);
                 windowClone.lower(deskletClone);
                 clone.desktopClones.push(windowClone);
             }
@@ -204,7 +197,7 @@ Cube.prototype = {
         for (let i = 0; i < workspaceWindows.length; i++) {
             workspaceWindows[i].i = workspaceWindows[i].get_stable_sequence();
             let windowClone = this.cloneMetaWindow(workspaceWindows[i]);
-            clone.add_actor(windowClone);
+            clone.add_child(windowClone);
             clone.workspaceWindowActors.push(windowClone);
         }
         clone.workspaceWindows = workspaceWindows;
@@ -214,7 +207,7 @@ Cube.prototype = {
             function (panel) {
                 // Is it a non-autohideable panel, or is it a visible, tracked
                 // chrome object? TODO: Make more human-readable the logic
-                // below in clone.add_actor().
+                // below in clone.add_child().
                 if ((panel.actor && !panel._hideable) || (panel &&
                     Main.layoutManager.isTrackingChrome(panel) &&
                     panel.visible)) {
@@ -226,11 +219,11 @@ Cube.prototype = {
                         Main.layoutManager.bottomMonitor.height -
                         panel.actor.height :
                         Main.layoutManager.primaryMonitor.y) : panel.y});
-                    chromeGroup.add_actor(chromeClone);
+                    chromeGroup.add_child(chromeClone);
                     chromeClone.raise_top();
                 }
             });
-        clone.add_actor(chromeGroup);
+        clone.add_child(chromeGroup);
         chromeGroup.raise_top();
         clone.chromeGroup = chromeGroup;
         clone.index = workspaceIndex;
@@ -257,9 +250,9 @@ Cube.prototype = {
         let windows = global.get_window_actors();
         for (let i = 0; i < windows.length; i++) {
             let meta_window = windows[i].get_meta_window();
-            if (meta_window.get_workspace().index() == workspaceIndex &&
-                !meta_window.minimized &&
-                meta_window.get_window_type() !== Meta.WindowType.DESKTOP) {
+            if (meta_window.get_workspace().index() === workspaceIndex
+                && !meta_window.minimized
+                && meta_window.get_window_type() !== Meta.WindowType.DESKTOP) {
                 workspaceWindows.push(meta_window);
             }
         }
@@ -308,7 +301,7 @@ Cube.prototype = {
         if (this.to != null) {
             from_workspace = this.to;
             needScale = false;
-            if (active_workspace.index() == new_workspace.index()) {
+            if (active_workspace.index() === new_workspace.index()) {
                 //this.bounce(from_workspace, direction);
                 this.is_animating = true;
                 this.from.hide();
@@ -318,19 +311,18 @@ Cube.prototype = {
             }
         } else {
             from_workspace = this.get_workspace_clone(active_workspace.index());
-            this.actor.add_actor(from_workspace);
+            this.actor.add_child(from_workspace);
         }
 
-        if (direction == this.last_direction) {
+        if (direction === this.last_direction) {
             if (this.from != null) {
-                to_workspace = this.get_workspace_clone_scaled
-                    (new_workspace.index(), direction);
-                this.actor.remove_actor(this.from);
+                to_workspace = this.get_workspace_clone_scaled(new_workspace.index(), direction);
+                this.actor.remove_child(this.from);
                 this.from.destroy();
             } else {
                 to_workspace = this.get_workspace_clone(new_workspace.index());
             }
-            this.actor.add_actor(to_workspace);
+            this.actor.add_child(to_workspace);
         } else {
             to_workspace = this.from;
         }
@@ -349,7 +341,7 @@ Cube.prototype = {
         from.show();
         to.show();
 
-        if (direction == Meta.MotionDirection.LEFT) {
+        if (direction === Meta.MotionDirection.LEFT) {
             let x_pos = 0;
             if (!needScale)
                 x_pos = global.stage.width * settings.pullaway;
@@ -385,7 +377,7 @@ Cube.prototype = {
         this.is_animating = true;
 
         let x_pos;
-        if (direction == Meta.MotionDirection.LEFT) {
+        if (direction === Meta.MotionDirection.LEFT) {
             x_pos = global.stage.width * settings.pullaway;
         } else {
             x_pos = global.stage.width * (1 - settings.pullaway);
@@ -414,7 +406,7 @@ Cube.prototype = {
 
         let angle_from;
         let angle_to;
-        if (direction == Meta.MotionDirection.LEFT) {
+        if (direction === Meta.MotionDirection.LEFT) {
             angle_from = 45;
             angle_to = -45;
         } else {
@@ -444,7 +436,7 @@ Cube.prototype = {
         to.raise_top();
         let x_pos;
         let angle_from;
-        if (direction == Meta.MotionDirection.LEFT) {
+        if (direction === Meta.MotionDirection.LEFT) {
             x_pos = global.stage.width * (1- settings.pullaway);
             angle_from = 90;
         } else {
@@ -473,7 +465,7 @@ Cube.prototype = {
         from.hide();
 
         let x_pos;
-        if (direction == Meta.MotionDirection.LEFT) {
+        if (direction === Meta.MotionDirection.LEFT) {
             to.move_anchor_point_from_gravity(Clutter.Gravity.EAST);
             to.set_position(global.stage.width * (1 - settings.pullaway),
                 global.stage.height / 2);
@@ -552,9 +544,8 @@ Cube.prototype = {
         let windows;
         let window;
         let event_state = Cinnamon.get_event_state(event);
-        let action = global.display.get_keybinding_action
-            (event.get_key_code(), event_state);
-        switch(action) {
+        let action = global.display.get_keybinding_action(event.get_key_code(), event_state);
+        switch (action) {
         case Meta.KeyBindingAction.MOVE_TO_WORKSPACE_LEFT:
              this.direction = Meta.MotionDirection.LEFT;
              workspace = global.screen.get_active_workspace().index();
@@ -587,15 +578,15 @@ Cube.prototype = {
         return true;
     },
 
-    _keyReleaseEvent: function(actor, event) {
+    _keyReleaseEvent: function() {
         let [x, y, mods] = global.get_pointer();
         let state = mods & this._modifierMask;
 
-        if (state == 0) {
+        if (state === 0) {
             if (this.is_animating)
                 this.destroy_requested = true;
             else
-            	this.onDestroy();
+                this.onDestroy();
         }
 
         return true;
