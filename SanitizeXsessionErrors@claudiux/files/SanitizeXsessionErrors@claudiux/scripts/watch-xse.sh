ed508109257e949a0d7d8ede81188af0a21cfa6a
claudiux <claude.clerc@gmail.com>
2020-09-16 16:48:45 +0200

    watch-xse.sh: Take into account the parameters.

--- SanitizeXsessionErrors@claudiux/files/SanitizeXsessionErrors@claudiux/scripts/watch-xse.sh
+++ SanitizeXsessionErrors@claudiux/files/SanitizeXsessionErrors@claudiux/scripts/watch-xse.sh
@@ -4,5 +4,5 @@ HEIGHT=$2
 LOGFILE=$HOME/.xsession-errors
 ICON=/usr/share/icons/gnome/48x48/emotes/face-glasses.png
 TITLE="${HOME}/.xsession-errors"
-tail --lines=+1 -f $LOGFILE | zenity --title "${TITLE}" --text-info --width 800 --height 960 --window-icon="${ICON}"
+tail --lines=+1 -f $LOGFILE | zenity --title "${TITLE}" --text-info --width ${WIDTH} --height ${HEIGHT} --window-icon="${ICON}"
 exit 0
