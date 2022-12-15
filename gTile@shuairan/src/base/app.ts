830689e3d3eb69477c027c678ceb190d5ad4e2b4
settenop <settenop@gmail.com>
2022-12-15 11:20:13 +0100

    revert: update current monitor before grid updates

--- gTile@shuairan/src/base/app.ts
+++ gTile@shuairan/src/base/app.ts
@@ -282,11 +282,13 @@ export class App implements IApp {
 
     this.focusMetaWindow = window;
 
-    this.currentMonitor = this.monitors[this.focusMetaWindow.get_monitor()];
-
+    
     if (!this.config.showGridOnAllMonitors) {
-        this.CurrentGrid.ChangeCurrentMonitor(this.monitors[this.focusMetaWindow.get_monitor()]);
+      this.CurrentGrid.ChangeCurrentMonitor(this.monitors[this.focusMetaWindow.get_monitor()]);
     }
+
+    this.currentMonitor = this.monitors[this.focusMetaWindow.get_monitor()];
+    
     this.focusMetaWindowPrivateConnections.push(
         ...this.platform.subscribe_to_focused_window_changes(this.focusMetaWindow, this.MoveUIActor)
     );
