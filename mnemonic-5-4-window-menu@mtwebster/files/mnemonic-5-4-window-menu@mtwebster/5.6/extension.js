ec506e5cdb5e25a8af4b6b49796f4836465bbc8a
Michael Webster <miketwebster@gmail.com>
2023-01-01 20:24:31 -0500

    mnemonic-5-4-window-menu@mtwebster: Disable in all but Cinnamon 5.4.
    Pre-5.4 it's not compatible, post-5.4 it's not necessary.

---
+++ mnemonic-5-4-window-menu@mtwebster/files/mnemonic-5-4-window-menu@mtwebster/5.6/extension.js
@@ -0,0 +1,14 @@
+// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*
+
+function init(metadata)
+{
+}
+
+function enable() 
+{
+    global.log("The extension 'mnemonic-5-4-window-menu@mtwebster' is only useful in Cinnamon 5.4. It is doing nothing currently. You can uninstall it.");
+}
+
+function disable() 
+{
+}
