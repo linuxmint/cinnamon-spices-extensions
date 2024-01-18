58430c52dfcd06b7b560fde8ba397cafab9df2ed
TobiZog <developer@zoghaib.de>
2024-01-18 20:17:52 +0100

    Cinnamon Dynamic Wallpaper v.2.0

--- cinnamon-dynamic-wallpaper@TobiZog/files/cinnamon-dynamic-wallpaper@TobiZog/5.4/image-configurator/data/enum.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class Source(Enum):
-	SELECTED = 0	# Load previous selected images
-	EXTRACT = 1		# Use a custom image set from a heic file
-	SET = 2			# Use an included image set
---
+++ cinnamon-dynamic-wallpaper@TobiZog/files/cinnamon-dynamic-wallpaper@TobiZog/5.4/scripts/dialogs.py
@@ -0,0 +1,61 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class Dialogs(Gtk.Window):
+  """ All used Gtk dialogs
+
+  Args:
+      Gtk (Gtk.Window): Window of Gtk application
+  """
+  def __init__(self) -> None:
+    super().__init__()
+
+
+  def source_folder_dialog(self) -> str:
+    """ Display a FileChooser dialog where the user choose a folder
+
+    Returns:
+        str: Absolute path to the selected folder
+    """
+    dialog = Gtk.FileChooserDialog(
+      title= "Please choose a folder with images",
+      parent=self,
+      action=Gtk.FileChooserAction.SELECT_FOLDER
+    )
+
+    dialog.add_buttons(
+      Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
+    )
+
+    dialog.set_default_size(800, 400)
+
+    response = dialog.run()
+
+    if response == Gtk.ResponseType.OK:
+      location = dialog.get_filename()
+    elif response == Gtk.ResponseType.CANCEL:
+      location = ""
+
+    dialog.destroy()
+
+    return location
+  
+  
+  def message_dialog(self, message: str):
+    """ Displaying a Gtk Message dialog to the user
+
+    Args:
+        message (str): Message which appear in the dialog
+    """
+    dialog = Gtk.MessageDialog(
+      transient_for=self,
+      flags=0,
+      message_type=Gtk.MessageType.INFO,
+      buttons=Gtk.ButtonsType.OK,
+      text=message
+    )
+
+    dialog.run()
+    dialog.destroy()
