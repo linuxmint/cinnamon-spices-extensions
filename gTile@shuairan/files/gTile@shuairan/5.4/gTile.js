05aaa530a012b2e9963fcc91706b1d38805ccba2
John Pycroft <andrew.ehlan.ace@gmail.com>
2025-04-27 22:35:46 +1000

    FEATURE Added hotkey for AutoTileMainList
    added a very hacky workaround to get an additional hotkey to trigger
    auto tiling.
    this was achieved directly in the JS rather than the TS as I wasn't sure
    how to access the functionality via TS namespacing.
    I was also unable to get the hotkey to register in the cinnamon shortcut
    interface for configuration, would appreciate a code review to point out
    necessary changes for merging.

--- gTile@shuairan/files/gTile@shuairan/5.4/gTile.js
+++ gTile@shuairan/files/gTile@shuairan/5.4/gTile.js
@@ -86,9 +86,11 @@ class Config {
         this.EnableHotkey = () => {
             this.DisableHotkey();
             Main.keybindingManager.addHotKey('gTile', this.hotkey, this.app.ToggleUI);
+            Main.keybindingManager.addHotKey('gTile2', '<Super>t', this.AutoTileMainAndList);
         };
         this.DisableHotkey = () => {
             Main.keybindingManager.removeHotKey('gTile');
+            Main.keybindingManager.removeHotKey('gTile2');
         };
         this.updateSettings = () => {
             for (const grid of this.app.Grids) {
@@ -126,6 +128,24 @@ class Config {
             this.DisableHotkey();
         };
         this.app = app;
+        this.AutoTileMainAndList = () => {
+            this.app.platform.reset_window(this.app.FocusMetaWindow);
+            let monitor = this.app.CurrentMonitor;
+            let [screenX, screenY, screenWidth, screenHeight] = getUsableScreenArea(monitor);
+            let windows = this.app.GetNotFocusedWindowsOfMonitor(monitor);
+            this.app.platform.move_resize_window(this.app.FocusMetaWindow, screenX, screenY, screenWidth / 2, screenHeight);
+            let winHeight = screenHeight / windows.length;
+            let countWin = 0;
+            for (let windowIdx in windows) {
+                let metaWindow = windows[windowIdx];
+                let newOffset = countWin * winHeight;
+                this.app.platform.reset_window(metaWindow);
+                this.app.platform.move_resize_window(metaWindow, screenX + screenWidth / 2, screenY + newOffset, screenWidth / 2, winHeight);
+                countWin++;
+            }
+        };
+
+        
         this.settings = new Settings.ExtensionSettings(this, 'gTile@shuairan');
         this.settings.bindProperty(Settings.BindingDirection.IN, 'hotkey', 'hotkey', this.EnableHotkey, null);
         this.settings.bindProperty(Settings.BindingDirection.OUT, 'lastGridRows', 'nbCols');
@@ -380,8 +400,8 @@ let AutoTileMainAndList = class AutoTileMainAndList extends ActionButton {
     constructor(app) {
         super('action-main-list', "auto_tile_0-symbolic");
         this._onButtonPress = () => {
-            if (!this.app.FocusMetaWindow)
-                return false;
+            // if (!this.app.FocusMetaWindow)
+                // return false;
             this.app.platform.reset_window(this.app.FocusMetaWindow);
             let monitor = this.app.CurrentMonitor;
             let [screenX, screenY, screenWidth, screenHeight] = getUsableScreenArea(monitor);
