b52201b9707da1c6c63251ba8b6aa41a573be5fc
Hilyxx <w.lebb@outlook.com>
2026-08-20 18:24:11 +0200

    fix
    revert locale path to align with Cinnamon Spices

--- cinnamon-screenshot@hilyxx/files/cinnamon-screenshot@hilyxx/translation.js
+++ cinnamon-screenshot@hilyxx/files/cinnamon-screenshot@hilyxx/translation.js
@@ -6,7 +6,7 @@ let currentUUID = null;
 // Initialize gettext with the provided UUID
 var initTranslation = function(uuid) {
     currentUUID = uuid;
-    Gettext.bindtextdomain(uuid, GLib.get_user_data_dir() + '/locale');
+    Gettext.bindtextdomain(uuid, GLib.get_home_dir() + '/.local/share/locale');
 }
 
 var _ = function(str) {
